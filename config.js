42f6a4cc5a68e58f1fbe9b9a919867ba674e97ad
s1059197 <pwittmer@gmail.com>
2026-05-28 20:53:18 -0400

    Wire Submit to Apps Script backend

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 window.UAP_IMAGE_API_BASE = "";
-window.UAP_REPORT_SUBMIT_URL = "";
+window.UAP_REPORT_SUBMIT_URL = "https://script.google.com/macros/s/AKfycbzB-CtgjUPGPkfbmoB1LA3yeFbwgZQKgJ-WvBy_17B8sQ-heFY1L6dhwCC0TUj7v6Wh/exec";
